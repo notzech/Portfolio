592889b201d9ef19e2fac87ace1879a45979eb84
notzech <zechariahjpohnson21@gmail.com>
2019-06-25 22:00:16 -0400

    L

--- assets/java.js
+++ assets/java.js
@@ -1,1 +1,10 @@
-
+console.log("e a sports");
+// function preload() {
+//   soundFormats('mp3', 'ogg');
+//   mySound = loadSound('ebicsax.mp3');
+// }
+//
+// function setup() {
+//   mySound.setVolume(50);
+//   mySound.play();
+// }
